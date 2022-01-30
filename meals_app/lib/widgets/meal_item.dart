25e870c3c34e7d4242b892c69eb3f6508fd0b782
Santiago Vargas <santiagovargasve@gmail.com>
2022-01-30 00:00:15 -0500

    ADD: Cards for meal items

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -8,6 +8,38 @@ class MealItem extends StatelessWidget {
   final Complexity complexity;
   final Affordability affordability;
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simple';
+
+      case Complexity.challenging:
+        return 'Challenging';
+
+      case Complexity.hard:
+        return 'Hard';
+
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.affordable:
+        return 'Affordable';
+
+      case Affordability.pricey:
+        return 'Pricey';
+
+      case Affordability.luxurious:
+        return 'Luxurious';
+
+      default:
+        return 'Unknown';
+    }
+  }
+
   const MealItem(
       {required this.title,
       required this.imageUrl,
@@ -39,10 +71,60 @@ class MealItem extends StatelessWidget {
                       topRight: Radius.circular(15),
                     ),
                     child: Image.network(imageUrl,
-                        height: 500,
+                        height: 300,
                         width: double.infinity,
                         fit: BoxFit.cover)),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    width: 220,
+                    color: Colors.black54,
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 6),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 6),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 6),
+                      Text(affordabilityText),
+                    ],
+                  )
+                ],
+              ),
             ),
           ],
         ),
